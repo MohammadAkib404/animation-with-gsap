e6e9625babb6221dffb3573e2c6a7b8850720adf
Mohammad Akib <akibo7394@gmail.com>
2025-07-31 20:40:18 +0530

    gsap.fromTo implementation

--- src/App.jsx
+++ src/App.jsx
@@ -14,19 +14,37 @@ function App() {
   //   })
   // }, [])
 
+  // useGSAP(() => {
+  //   gsap.from("#green-box", {
+  //     x: 250,
+  //     repeat: -1,
+  //     yoyo: true,
+  //     rotation: 360,
+  //     duration: 3,
+  //     ease: 'power1.inOut'
+  //   })
+  // }, [])
+
   useGSAP(() => {
-    gsap.from("#green-box", {
-      x: 250,
-      repeat: -1,
-      yoyo: true,
-      rotation: 360,
-      duration: 3,
-      ease: 'power1.inOut'
-    })
+    gsap.fromTo("#green-box",
+      {
+        x: 0,
+        y: 50,
+        borderRadius: '0%',
+      },
+      {
+        x: 1000,
+        repeat: -1,
+        borderRadius: '100%',
+        yoyo: true,
+        rotation: 360,
+        duration: 2,
+        ease: 'power1.inOut'
+      })
   }, [])
 
   return (
-    <div className='h-full flex justify-center items-center'>
+    <div className=''>
       <div id='green-box' className='w-50 h-50 bg-green-400 rounded-2xl border'></div>
     </div>
   )
